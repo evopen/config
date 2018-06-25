ea4be7b616a0d85a24dcb970fe794323f135660b
evopen <520dhh@gmail.com>
2018-06-25 23:20:40 +0800

    add neovim shortcut

--- config/config.fish
+++ config/config.fish
@@ -3,6 +3,7 @@ export https_proxy=http://127.0.0.1:1087
 export GIT_AUTHOR_EMAIL=520dhh@gmail.com
 source /usr/local/miniconda3/etc/fish/conf.d/conda.fish
 alias em="emacsclient -c"
+alias vm="nvim"
 export GPG_TTY=(tty)
 
 if not test -n "$TMUX"
