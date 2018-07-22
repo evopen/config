0fdd36f18aaddcb1010dab91676d5fdff1fa8ed5
evopen <520dhh@gmail.com>
2018-07-22 14:44:17 +0800

    change proxy port to 12333

--- config/config.fish
+++ config/config.fish
@@ -1,7 +1,6 @@
-export http_proxy=http://127.0.0.1:1087
-export https_proxy=http://127.0.0.1:1087
+export http_proxy=http://127.0.0.1:12333
+export https_proxy=http://127.0.0.1:12333
 export GIT_AUTHOR_EMAIL=520dhh@gmail.com
-source /usr/local/miniconda3/etc/fish/conf.d/conda.fish
 alias em="emacsclient -c"
 alias vm="nvim"
 export GPG_TTY=(tty)
