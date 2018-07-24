424d6a456dca7518b77598b9e3548f49c91d321f
evopen <520dhh@gmail.com>
2018-07-24 12:27:34 +0800

    run restore script everywhere

--- restore.sh
+++ restore.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 mkdir -p ~/.config/fish
-cp ./config/spacemacs ~/.spacemacs
-cp ./config/config.fish ~/.config/fish/config.fish
-cp ./config/profile ~/.profile
-cp ./config/clang-format ~/.clang-format
-cp ./config/init.toml ~/.SpaceVim.d/init.toml
-cp ./config/gitconfig ~/.gitconfig
+cp $DIR/config/spacemacs ~/.spacemacs
+cp $DIR/config/config.fish ~/.config/fish/config.fish
+cp $DIR/config/profile ~/.profile
+cp $DIR/config/clang-format ~/.clang-format
+cp $DIR/config/init.toml ~/.SpaceVim.d/init.toml
+cp $DIR/config/gitconfig ~/.gitconfig
 
 if [ "$1" = "docker" ]; then
   sed -i 's/127.0.0.1/host.docker.internal/g' ~/.config/fish/config.fish
