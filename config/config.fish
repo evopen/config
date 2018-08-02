bd7e9412a7a32f02f2ca0120d65b209a95041a63
evopen <520dhh@gmail.com>
2018-08-02 09:53:28 +0800

    fix shit

--- config/config.fish
+++ config/config.fish
@@ -1,7 +1,7 @@
 export http_proxy=http://127.0.0.1:12333
 export https_proxy=http://127.0.0.1:12333
 export GIT_AUTHOR_EMAIL=520dhh@gmail.com
-alias em="emacsclient -c"
+alias em="emacsclient -nw -c"
 alias vm="nvim"
 export GPG_TTY=(tty)
 
